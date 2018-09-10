0eeaf6d57aee41f50b7e7e79fc3447ef104d91fb
ElonMusk2018 <42662533+ElonMusk2018@users.noreply.github.com>
2018-09-10 10:14:32 -0700

    Add files via upload

---
+++ num1.py
@@ -0,0 +1,5 @@
+# nums1.py  cwc
+
+x = 1000000000000000   # int
+y = 2.82345354  # float
+z = 1j   # complex
---
+++ var2.py
@@ -0,0 +1,7 @@
+# var2.py
+a = 10 # integer (int)
+b = -5  #integer (int)
+s2 = 1.414 # float
+s = "This is a string of characters" # string
+s3= 'How about this' # string
+print (a,b,s2,s,s3)
