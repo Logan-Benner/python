01860a24890b72f0123dc863a42f85fbd6b996ae
Logan-Benner <42662533+Logan-Benner@users.noreply.github.com>
2019-10-02 09:24:02 -0700

    Add files via upload

--- basecon.py
+++ basecon.py
@@ -1,24 +1,27 @@
 #basecon lb
-
 def bincon(num,addSpace):
 	n = num
 	s = addSpace
-	#print(n,s)
-	print(n," ",end="")
+	#print(n,s) #debug
+	print(n," = ",end="")
 	d = 128
 	binString ="" #create a string called binString
 	for i in range(0,8):
 		q = int(n / d)
 		r = int(n % d)
-		print(d,q,r)# debug line
 		n = r
 		d = int(d / 2)
 		binString = binString+str(q)
-	print(binString)
+		if(s == 1 and i == 3):
+			binString = binString + " "
+	#Print(binString)
+	return binString
 	
 def main():
-		bincon(191,0)
-		bincon(7,0)
-		bincon(15,0)
-		bincon(31,0)
+		bs = ""
+		bs = bincon(152,1)
+		print (152,bs)
+		bs = bincon(191,1)
+		bs = bincon(7,1)
+		
 main()
