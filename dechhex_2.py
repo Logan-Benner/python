816abd1641c1477ebac9ba232bb35bdfdb6b1021
Logan-Benner <42662533+Logan-Benner@users.noreply.github.com>
2019-10-04 10:13:53 -0700

    Add files via upload

---
+++ dechhex_2.py
@@ -0,0 +1,15 @@
+# dechex.py lb
+def hexcon(num):
+	key = "0123456789abcdef"
+	h16 = int(num/16)
+	h1 = num % 16
+	h = key[h16]+ key[h1]
+	return h
+
+def main():
+		hs = ""
+		for i in range(0, 256):
+			hs = hexcon(i)
+			print(i,hs)
+			
+main()
---
+++ keys.py
@@ -0,0 +1,9 @@
+#keys.py Kenny lb
+key = "0123456789abcdef"
+for i in range (0,16):
+	print(key[i]," ",end=""0
+	
+h16 = 9
+h1 = 13
+print("\* * * * * * *")
+print(9*16
