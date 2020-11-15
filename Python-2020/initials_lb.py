49387231b25f232769e2ad00c42046f3f38f5eee
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2020-11-14 17:39:36 -0800

    Create initials_lb.py

---
+++ Python-2020/initials_lb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# functionDemo.py
+def printStars(n):
+    print(n," ",end="")
+    print("**|***|--|********")
+    print("*****|***|*-|*****")
+    print("*****|__*|--|*****")
+    
+def main():
+    printStars(1)# calling a function printStars()
+    printStars(2)
+    printStars(3)
+    
+main()
+
+'''
+python functions
+- defining a function
+- calling a function
+- function arguments (parameters)
+'''
+
