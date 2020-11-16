923ccde7512d4f15c41a94ef19bb6c08c9902466
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2020-11-15 19:34:20 -0800

    Create asc2_a.py

---
+++ Python-2020/asc2_a.py
@@ -0,0 +1,21 @@
+#python3 asc2-a.py lb
+#divertido
+#This code will get saved to github.com/username/python repository
+#Please start typing this code
+#variables
+i1 = 33
+i2 = 48
+i3 = 65
+i4 = i3+25
+i5 = 97
+#output
+print("integers")
+print(i1,i2,i3,i4,i5)
+#Type cast variables
+c1 = chr(i1)
+c2 = chr(i2)
+c3 = chr(i3)
+c4 = chr(i4)
+c5 = chr(i5)
+print("characters")
+print(c1,c2,c3,c4,c5)
