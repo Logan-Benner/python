9e1d895765020ee10c0711901f766988fa1db903
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2020-11-15 17:12:19 -0800

    Create dex-hex-1.py

---
+++ Python-2020/dex-hex-1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#Base 10 to Base 10 lb dex-hex-1.py
+print ("DECIMAL", 0,"= HEXADECIMAL", hex(0))
+print ("DECIMAL", 1,"= HEXADECIMAL", hex(1))
+print ("DECIMAL", 2,"= HEXADECIMAL", hex(2))
+print ("DECIMAL", 3,"= HEXADECIMAL", hex(3))
+print ("DECIMAL", 4,"= HEXADECIMAL", hex(4))
+print ("DECIMAL", 5,"= HEXADECIMAL", hex(5))
+print ("DECIMAL", 6,"= HEXADECIMAL", hex(6))
+print ("DECIMAL", 7,"= HEXADECIMAL", hex(7))
+print ("DECIMAL", 8,"= HEXADECIMAL", hex(8))
+print ("DECIMAL", 9,"= HEXADECIMAL", hex(9))
+print ("DECIMAL", 10,"= HEXADECIMAL", hex(10))
+print ("DECIMAL", 11,"= HEXADECIMAL", hex(11))
+print ("DECIMAL", 12,"= HEXADECIMAL", hex(12))
+print ("DECIMAL", 13,"= HEXADECIMAL", hex(13))
+print ("DECIMAL", 14,"= HEXADECIMAL", hex(14))
+print ("DECIMAL", 15,"= HEXADECIMAL", hex(15))
+print ("DECIMAL", 16,"= HEXADECIMAL", hex(16))
+'''
+16^7 16^6 16^5 16^4 16^3 16^2 16^1 16^0
+128  64  32  16  8   4   2   1
+	16^7	   16^6	     16^5	  16^4	16^3	16^2	16^0
+268,435,456 10,777,216 1,048,576 65,536 4096 256 16 1
+DEC (10 DIGITS -> 0 to 9) HEX (16 DIGITS -> 0 to r)
+0	0000 0000
+1	0000 0001
+2	0000 0010
+3	0000 0011
+4	0000 0100
+5	0000 0101
+6	0000 0110
+7	0000 0111
+8	0000 1000
+9	0000 1001
+10  0000 1010
+11  0000 1001
+12  0000 1100
+13  0000 1101
+14  0000 1101
+15  0000 1111
+16  0001 0000
+'''
+   
