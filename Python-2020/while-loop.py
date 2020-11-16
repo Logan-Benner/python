d85126a71c329819a2c9126ebee8c431b71f0fb5
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2020-11-15 17:07:04 -0800

    Create while-loop.py

---
+++ Python-2020/while-loop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# while-loop.py
+count = 0
+while(count < 256):
+	print(str(count)+" ",end="")
+	count = count + 1
+	if(count % 20 == 0):
+		print(" + ")
+'''
+scope = encapsulation
+concatention
+'''
